75b2695c8146e27d1dbbb94ceea3e970cb074fd7
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-06-14 22:32:43 +0530

    Added employee exception file.

---
+++ employee-management/src/main/java/com/employee_management/exception/EmployeeException.java
@@ -0,0 +1,12 @@
+package com.employee_management.exception;
+
+public class EmployeeException extends RuntimeException {
+
+    public EmployeeException(String message) {
+        super(message);
+    }
+
+    public EmployeeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
